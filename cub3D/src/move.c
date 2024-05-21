f50f1893a9365122c5448b1f8f8c5ac9405a1607
Aaron Espinosa Garcés <aaronespinosatomas@gmail.com>
2024-05-21 21:01:47 +0200

    collision works

--- cub3D/src/move.c
+++ cub3D/src/move.c
@@ -6,29 +6,32 @@
 /*   By: aaespino <aaespino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 16:31:55 by aaespino          #+#    #+#             */
-/*   Updated: 2024/05/21 20:02:54 by aaespino         ###   ########.fr       */
+/*   Updated: 2024/05/21 20:28:51 by aaespino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-static int	wall_found(int *scr, t_player *player)
+static int	wall_found(int *scr, float x, float y)
 {
 	int	pos;
+	int pos_x;
+	int pos_y;
 
-	pos = 0;
-	if (player->n[X] != 0 && player->n[Y] != 0)
-	{
-		pos = player->n[Y] * WIDTH + player->n[X];
-		if (scr[pos] == 0xFF0000)
-			return (1);
-	}
+	pos_x = (int)(x);
+	pos_y = (int)(y);
+	pos = pos_y * WIDTH + pos_x;
+	if ((pos_x >= 0 && pos_y >= 0) 
+		&& (pos_x < WIDTH && pos_y < HEIGHT) 
+		&& scr[pos] == WALL)
+		return (1);
 	return (0);
 }
 
 void	move_front(t_data *info)
 {
-	if (!wall_found(info->mlx->img.img_adr, &info->player))
+	if (!wall_found(info->mlx->img.img_adr, info->player.x + 
+		info->player.dx, info->player.y + info->player.dy))
 	/*if (check_collision(info,
 			info->player.x + info->player.dx * SPEED,
 			info->player.y + info->player.dy * SPEED))*/
@@ -37,21 +40,18 @@ void	move_front(t_data *info)
 		info->player.y += info->player.dy;
 		info->minipl.x += info->minipl.dx;
 		info->minipl.y += info->minipl.dy;
-		info->player.n[X] = info->player.x;
-		info->player.n[Y] = info->player.y;
 	}
 }
 
 void	move_back(t_data *info)
 {
-	if (!wall_found(info->mlx->img.img_adr, &info->player))
+	if (!wall_found(info->mlx->img.img_adr, info->player.x - 
+		info->player.dx, info->player.y - info->player.dy))
 	{
 		info->player.x -= info->player.dx;
 		info->player.y -= info->player.dy;
 		info->minipl.x -= info->minipl.dx;
 		info->minipl.y -= info->minipl.dy;
-		info->player.n[X] = info->player.x;
-		info->player.n[Y] = info->player.y;
 	}
 }
 
@@ -64,14 +64,13 @@ void	move_right(t_data *info)
 	new_dir = angle_correction(90 - info->player.dir);
 	new_dx = cos(deg_to_rad(new_dir));
 	new_dy = sin(deg_to_rad(new_dir));
-	if (!wall_found(info->mlx->img.img_adr, &info->player))
+	if (!wall_found(info->mlx->img.img_adr, info->player.x + 
+		new_dx * 6, info->player.y + new_dy * 6))
 	{
 		info->player.x += new_dx * 6;
 		info->player.y += new_dy * 6;
 		info->minipl.x += new_dx;
 		info->minipl.y += new_dy;
-		info->player.n[X] = info->player.x;
-		info->player.n[Y] = info->player.y;
 	}
 }
 
@@ -90,14 +89,13 @@ void	move_left(t_data *info)
 	new_dir = angle_correction(90 - info->player.dir);
 	new_dx = cos(deg_to_rad(new_dir));
 	new_dy = sin(deg_to_rad(new_dir));
-	if (!wall_found(info->mlx->img.img_adr, &info->player))
+	if (!wall_found(info->mlx->img.img_adr, info->player.x - 
+		new_dx * 6, info->player.y - new_dy * 6))
 	{
 		info->player.x -= new_dx * 6;
 		info->player.y -= new_dy * 6;
 		info->minipl.x -= new_dx;
 		info->minipl.y -= new_dy;
-		info->player.n[X] = info->player.x;
-		info->player.n[Y] = info->player.y;
 	}
 }
 
