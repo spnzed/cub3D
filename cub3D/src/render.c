59d226801dc6382784d30be1eb8ef0ef1ef154a1
Aaron Espinosa Garcés <aaronespinosatomas@gmail.com>
2024-05-27 18:22:02 +0200

    BIG

--- cub3D/src/render.c
+++ cub3D/src/render.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   render.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: erosas-c <erosas-c@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aaespino <aaespino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 15:46:35 by aaespino          #+#    #+#             */
-/*   Updated: 2024/05/23 17:44:27 by erosas-c         ###   ########.fr       */
+/*   Updated: 2024/05/27 18:18:05 by aaespino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,8 +65,8 @@ void	handle_key(t_data *info)
 void	new_scene(t_data *info)
 {
 	get_bigmap2d(info);
-	mini_map(info);
 	cast_rays(info);
+	mini_map(info);
 }
 
 int	render(t_data *info)
--- cub3D/src/wall.c
+++ cub3D/src/wall.c
@@ -6,7 +6,7 @@
 /*   By: aaespino <aaespino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 19:04:07 by aaespino          #+#    #+#             */
-/*   Updated: 2024/05/27 17:57:49 by aaespino         ###   ########.fr       */
+/*   Updated: 2024/05/27 18:16:45 by aaespino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,21 +32,22 @@ void	draw_wall(t_data *info, int *scr, int ang, int i)
 
 	(void)scr;
 	distance = remove_fish(info->player.dir, ang, info->ray[i].len);
-	height = (SCALE * 320) / (distance); // Calcula la altura de la línea
-	if (height > 320) 
-    	height = 320;
-	offset = 160 - (height / 2); // Calcula el offset de la línea
+	height = (SCALE * HEIGHT) / (distance); // Calcula la altura de la línea
+	if (height > HEIGHT) 
+    	height = HEIGHT;
+	offset = (HEIGHT / 2) - (height / 2); // Calcula el offset de la línea
 
-	t_point wall_pts[2] = {{i * 1 + 530, offset}, {i * 1 + 530, offset + height}};
+	int x_position = (i * WIDTH) / (WIDTH / 2); // Distribuye las líneas uniformemente a lo largo del ancho de la pantalla
+	t_point wall_pts[2] = {{x_position, offset}, {x_position, offset + height}};
 
 	if (info->ray[i].wall_or == 'N')
-    	draw_line(info->mlx->img.img_adr, wall_pts, 0xff0000, 1); // Red
+    	draw_line(info->mlx->img.img_adr, wall_pts, 0xff0000, 2); // Red
 	else if (info->ray[i].wall_or == 'S')
-    	draw_line(info->mlx->img.img_adr, wall_pts, 0x00ff00, 1); // Green  
+    	draw_line(info->mlx->img.img_adr, wall_pts, 0x00ff00, 2); // Green  
 	else if (info->ray[i].wall_or == 'W')
-    	draw_line(info->mlx->img.img_adr, wall_pts, 0x0000ff, 1); // Blue	  
+    	draw_line(info->mlx->img.img_adr, wall_pts, 0x0000ff, 2); // Blue	  
 	else if (info->ray[i].wall_or == 'E')
-    	draw_line(info->mlx->img.img_adr, wall_pts, 0xffff00, 1); // Yellow  
+    	draw_line(info->mlx->img.img_adr, wall_pts, 0xffff00, 2); // Yellow  
 	// Calcula las coordenadas de textura
 	// float tex_x = (float)i / (float)WIDTH * info->mlx->north.width;
 	// int tex_y = 0; // Puedes calcular la coordenada y de textura basándote en el ángulo o la distancia si es necesario
