af51ae70fbfcacfde73b2545b5f33703f7204a9e
Aaron Espinosa <95354392+spnzed@users.noreply.github.com>
2024-06-22 14:59:00 +0200

    SOLVED, idk why

--- cub3D/src/wall.c
+++ cub3D/src/wall.c
@@ -6,7 +6,7 @@
 /*   By: aaronespinosa <aaronespinosa@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 19:04:07 by aaespino          #+#    #+#             */
-/*   Updated: 2024/06/22 13:51:29 by aaronespino      ###   ########.fr       */
+/*   Updated: 2024/06/22 14:58:19 by aaronespino      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,20 +26,21 @@ float	remove_fish(int player_dir, int ray_ang, int ray_len)
 
 void draw_text(t_img texture, int *scr, int offset, int height, int tex_x, int i)
 {
-	// Calcula las coordenadas de textura
-    // float tex_x = (float)i / (float)WIDTH * texture.width;
-    float tex_y;
+	float tex_y;
     int tex_pixel;
+    int texture_height = texture.height;
+    int texture_width = texture.width;
 
-	for (int y = offset; y < offset + height; y++) // Itera sobre la altura completa de la pared
-    {
-        // Ajusta las coordenadas de textura para que se mapeen correctamente a la altura de la pared
-        tex_y = ((float)(y - offset) / (float)height) * texture.width;
-        // Calcula la coordenada y de textura correspondiente a este píxel
-        tex_pixel = ((int)tex_y * texture.width + (int)tex_x);
-        // Copia el color del píxel de la textura al buffer de pantalla
-        if (tex_x >= 0 && tex_x < texture.width && tex_y >= 0 && tex_y < texture.width)
+    int y_start = offset < 0 ? 0 : offset;
+    int y_end = offset + height >= HEIGHT ? HEIGHT : offset + height;
+
+    for (int y = y_start; y < y_end; y++) {
+        tex_y = ((float)(y - offset) / (float)height) * texture_height;
+        tex_pixel = ((int)tex_y * texture_width + (int)tex_x);
+
+        if (tex_x >= 0 && tex_x < texture_width && tex_y >= 0 && tex_y < texture_height) {
             scr[y * WIDTH + i] = texture.img_adr[tex_pixel];
+        }
     }
 }
 
@@ -51,7 +52,8 @@ static void draw_wallcol(t_data *info, int *scr, int ang, int i)
 
     distance = remove_fish(info->player.dir, ang, info->ray[i].len);
     height = (SCALE * HEIGHT) / (distance); // Calcula la altura de la línea
-    offset = (HEIGHT / 2) - (height / 2); // Calcula el offset de la línea
+    offset = (HEIGHT / 2) - (height / 2);   // Calcula el offset de la línea
+
 	if (info->ray[i].wall_or == 'N')
 		draw_text(info->mlx->north, scr, offset, height, info->ray[i].text_p, i);
 	else if (info->ray[i].wall_or == 'S')
