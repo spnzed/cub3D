59db4580b07e96f10a240d97ee44acee2a276afe
Aaron Espinosa <95354392+spnzed@users.noreply.github.com>
2024-06-25 20:00:49 +0200

    norm almost

--- cub3D/src/wall.c
+++ cub3D/src/wall.c
@@ -5,68 +5,65 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aaronespinosa <aaronespinosa@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/22 19:04:07 by aaespino          #+#    #+#             */
-/*   Updated: 2024/06/22 14:58:19 by aaronespino      ###   ########.fr       */
+/*   Created: 2024/06/25 19:29:32 by aaronespino       #+#    #+#             */
+/*   Updated: 2024/06/25 19:55:14 by aaronespino      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-float degToRad(int a) { return a*M_PI/180.0;}
-
 float	remove_fish(int player_dir, int ray_ang, int ray_len)
 {
-	int rem_fish;
-	float distance;
+	int		rem_fish;
+	float	distance;
 
 	rem_fish = player_dir - ray_ang;
-	distance = ray_len * cos(degToRad(rem_fish)); // Corrige "fisheye"
+	distance = ray_len * cos(deg_to_rad(rem_fish));
 	return (distance);
 }
 
-void draw_text(t_img texture, int *scr, int offset, int height, int tex_x, int i)
+void	draw_text(t_img texture, int *scr, int offset, int height, int tex_x, int i)
 {
-	float tex_y;
-    int tex_pixel;
-    int texture_height = texture.height;
-    int texture_width = texture.width;
+	float	tex_y;
+	int		tex_pixel;
+	int		y_end;
+	int		j;
 
-    int y_start = offset < 0 ? 0 : offset;
-    int y_end = offset + height >= HEIGHT ? HEIGHT : offset + height;
-
-    for (int y = y_start; y < y_end; y++) {
-        tex_y = ((float)(y - offset) / (float)height) * texture_height;
-        tex_pixel = ((int)tex_y * texture_width + (int)tex_x);
-
-        if (tex_x >= 0 && tex_x < texture_width && tex_y >= 0 && tex_y < texture_height) {
-            scr[y * WIDTH + i] = texture.img_adr[tex_pixel];
-        }
-    }
+	y_end = offset + height >= HEIGHT ? HEIGHT : offset + height;
+	j = 0;
+	while (j < y_end)
+	{
+		tex_y = ((float)(j - offset) / (float)height) * texture.height;
+		tex_pixel = ((int)tex_y * texture.width + (int)tex_x);
+		if (tex_x >= 0 && tex_x < texture.width && tex_y >= 0 \
+			&& tex_y < texture.height)
+			scr[j * WIDTH + i] = texture.img_adr[tex_pixel];
+		j++;
+	}
 }
 
-static void draw_wallcol(t_data *info, int *scr, int ang, int i)
+static void	draw_wallcol(t_data *info, int *scr, int ang, int i)
 {
-    float distance;
-    int height;
-    int offset;
+	float	distance;
+	int		height;
+	int		off;
 
-    distance = remove_fish(info->player.dir, ang, info->ray[i].len);
-    height = (SCALE * HEIGHT) / (distance); // Calcula la altura de la línea
-    offset = (HEIGHT / 2) - (height / 2);   // Calcula el offset de la línea
-
+	distance = remove_fish(info->player.dir, ang, info->ray[i].len);
+	height = (SCALE * HEIGHT) / (distance);
+	off = (HEIGHT / 2) - (height / 2);
 	if (info->ray[i].wall_or == 'N')
-		draw_text(info->mlx->north, scr, offset, height, info->ray[i].text_p, i);
+		draw_text(info->mlx->north, scr, off, height, info->ray[i].text_p, i);
 	else if (info->ray[i].wall_or == 'S')
-		draw_text(info->mlx->south, scr, offset, height, info->ray[i].text_p, i);
+		draw_text(info->mlx->south, scr, off, height, info->ray[i].text_p, i);
 	else if (info->ray[i].wall_or == 'W')
-		draw_text(info->mlx->west, scr, offset, height, info->ray[i].text_p, i);
+		draw_text(info->mlx->west, scr, off, height, info->ray[i].text_p, i);
 	else if (info->ray[i].wall_or == 'E')
-		draw_text(info->mlx->east, scr, offset, height, info->ray[i].text_p, i);
+		draw_text(info->mlx->east, scr, off, height, info->ray[i].text_p, i);
 }
 
-void draw_walls(t_data *info)
+void	draw_walls(t_data *info)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (i < WIDTH)
