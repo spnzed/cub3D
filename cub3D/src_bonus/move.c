995ee21b215ce6bf0be6115788e8c2c749e5d047
Aaron Espinosa <95354392+spnzed@users.noreply.github.com>
2024-06-27 21:22:41 +0200

    Add files via upload

--- cub3D/src_bonus/move.c
+++ cub3D/src_bonus/move.c
@@ -28,30 +28,38 @@ static int	wall_found(float x, float y, t_map *m)
 
 void	move_front(t_player *p, t_player *minipl, t_map *m)
 {
-	if (!wall_found(p->x + p->dx, p->y + p->dy, m)
-		&& !wall_found(p->x + p->dx * 2, p->y + p->dy * 2, m)
-		&& !wall_found(p->x + p->dx * 3, p->y + p->dy * 3, m)
-		&& !wall_found(p->x + p->dx * 4, p->y + p->dy * 4, m)
+	float	ini_x;
+
+	ini_x = p->x;
+	if (!wall_found(p->x + p->dx * 15, p->y, m)
 		&& !move_concave(p->dx, p->dy, p, m))
 	{
 		p->x += p->dx * SPEED;
-		p->y += p->dy * SPEED;
 		minipl->x += p->dx / 4 * SPEED;
+	}
+	if (!wall_found(ini_x, p->y + p->dy * 15, m)
+		&& !move_concave(p->dx, p->dy, p, m))
+	{
+		p->y += p->dy * SPEED;
 		minipl->y += p->dy / 4 * SPEED;
 	}
 }
 
 void	move_back(t_player *p, t_player *minipl, t_map *m)
 {
-	if (!wall_found(p->x - p->dx, p->y - p->dy, m)
-		&& !wall_found(p->x - p->dx * 2, p->y - p->dy * 2, m)
-		&& !wall_found(p->x - p->dx * 3, p->y - p->dy * 3, m)
-		&& !wall_found(p->x - p->dx * 4, p->y - p->dy * 4, m)
+	float	ini_x;
+
+	ini_x = p->x;
+	if (!wall_found(p->x - p->dx * 15, p->y, m)
 		&& !move_concave((p->dx * -1), (p->dy * -1), p, m))
 	{
 		p->x -= p->dx * SPEED;
-		p->y -= p->dy * SPEED;
 		minipl->x -= p->dx / 4 * SPEED;
+	}
+	if (!wall_found(ini_x, p->y - p->dy * 15, m)
+		&& !move_concave((p->dx * -1), (p->dy * -1), p, m))
+	{
+		p->y -= p->dy * SPEED;
 		minipl->y -= p->dy / 4 * SPEED;
 	}
 }
@@ -61,19 +69,22 @@ void	move_right(t_player *p, t_player *minipl, t_map *m)
 	float	new_dir;
 	float	new_dx;
 	float	new_dy;
+	float	ini_x;
 
+	ini_x = p->x;
 	new_dir = angle_correction(90 - p->dir);
 	new_dx = cos(deg_to_rad(new_dir));
 	new_dy = sin(deg_to_rad(new_dir));
-	if (!wall_found(p->x + new_dx, p->y + new_dy, m)
-		&& !wall_found(p->x + new_dx * 2, p->y + new_dy * 2, m)
-		&& !wall_found(p->x + new_dx * 3, p->y + new_dy * 3, m)
-		&& !wall_found(p->x + new_dx * 4, p->y + new_dy * 4, m)
+	if (!wall_found(p->x + new_dx * 0.5, p->y, m)
 		&& !move_concave(new_dx, new_dy, p, m))
 	{
 		p->x += new_dx * SPEED;
-		p->y += new_dy * SPEED;
 		minipl->x += new_dx / 4 * SPEED;
+	}
+	if (!wall_found(ini_x, p->y + new_dy * 0.5, m)
+		&& !move_concave(new_dx, new_dy, p, m))
+	{
+		p->y += new_dy * SPEED;
 		minipl->y += new_dy / 4 * SPEED;
 	}
 }
@@ -83,19 +94,22 @@ void	move_left(t_player *p, t_player *minipl, t_map *m)
 	float	new_dir;
 	float	new_dx;
 	float	new_dy;
+	float	ini_x;
 
+	ini_x = p->x;
 	new_dir = angle_correction(90 - p->dir);
 	new_dx = cos(deg_to_rad(new_dir));
 	new_dy = sin(deg_to_rad(new_dir));
-	if (!wall_found(p->x - new_dx, p->y - new_dy, m)
-		&& !wall_found(p->x - new_dx * 2, p->y - new_dy * 2, m)
-		&& !wall_found(p->x - new_dx * 3, p->y - new_dy * 3, m)
-		&& !wall_found(p->x - new_dx * 4, p->y - new_dy * 4, m)
+	if (!wall_found(p->x - new_dx * 0.5, p->y, m)
 		&& !move_concave((new_dx * -1), (new_dy * -1), p, m))
 	{
 		p->x -= new_dx * SPEED;
-		p->y -= new_dy * SPEED;
 		minipl->x -= new_dx / 4 * SPEED;
+	}
+	if (!wall_found(ini_x, p->y - new_dy * 0.5, m)
+		&& !move_concave((new_dx * -1), (new_dy * -1), p, m))
+	{
+		p->y -= new_dy * SPEED;
 		minipl->y -= new_dy / 4 * SPEED;
 	}
 }
